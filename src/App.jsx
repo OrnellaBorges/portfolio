5cb7b626e32bf008a0d100e2768dc8755223cc69
Ornella B <borges.ornella@gmail.com>
2023-07-24 18:32:43 +0200

    apps components imports component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,30 @@ import { useState } from "react";
 //import viteLogo from "/vite.svg";
 import "./App.css";
 
+
 //IMPORT COMPONENTS
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Porfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <>
       <Header/>
+      <Nav/>
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact/>
+      <Footer/>
     </>
   );
 }
