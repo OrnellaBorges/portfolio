998e87fdc1206f4457fade5fca0a6e3ee28a4f03
Ornella B <borges.ornella@gmail.com>
2023-08-09 22:24:22 +0200

    portfolio component completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import './portfolio.css'
+import {data} from '../../data/projectData'
 
 const Portfolio = () => {
   return (
     <>
-        <section id='portfolio'>Portfolio</section>
-        <ul>
-            <li className="element">cghdjhgcfhjds</li>
-        </ul>
+        <section id='portfolio'>
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+            <div className="container portfolio__container">
+                {
+                    data.map(({id, image, title, github, demo})=> {
+                        return (
+                            <article key={id} className="portfolio__item">
+                                <div className="portfolio__item-image">
+                                    <img src={image} alt={title}/>
+                                </div>
+                                <h3>{title}</h3>
+                                <div className="portfolio__item-cta">
+                                    <a href={github} className='btn'>Github</a>
+                                    <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+            </div>
+        </section>
+
     </>
 )}
 
---
+++ src/data/projectData.js
@@ -0,0 +1,37 @@
+import IMG1 from '../assets/portfolio1.jpg'
+import IMG2 from '../assets/portfolio2.jpg'
+import IMG3 from '../assets/portfolio3.jpg'
+import IMG4 from '../assets/portfolio4.jpg'
+import IMG5 from '../assets/portfolio5.png'
+import IMG6 from '../assets/portfolio6.jpg'
+
+export const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title : 'title image',
+        github: 'https://github.com',
+        demo:'"https://dribbble.com/Alien_pixels'
+    },
+    {
+        id: 2,
+        image: IMG1,
+        title : 'title image',
+        github: 'https://github.com',
+        demo:'"https://dribbble.com/Alien_pixels'
+    },
+    {
+        id: 3,
+        image: IMG1,
+        title : 'title image',
+        github: 'https://github.com',
+        demo:'"https://dribbble.com/Alien_pixels'
+    },
+    {
+        id: 4,
+        image: IMG1,
+        title : 'title image',
+        github: 'https://github.com',
+        demo:'"https://dribbble.com/Alien_pixels'
+    }]
+
