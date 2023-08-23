3a395c46cc6c3b07fd4660cd808f0abff98548a3
Mikael B <borges.mikael@gmail.com>
2023-08-23 16:29:46 +0200

    Improvement header component and style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,41 +1,27 @@
-import React from 'react'
 //import { styles } from './header.css' // CSS MODULE VERSION
-import "./header.css"
+import "./header.css";
 
 // IMPORT COMPONENTS
-import CTA from './CTA'
-import Ornella from '../../assets/Ornella-nb.jpg'
-import HeaderSocials from './HeaderSocials'
+import CTA from "./CTA";
+import OrnellaMobile from "../../assets/Ornella-nb-mobile.jpg";
+import Ornella from "../../assets/Ornella-nb.jpg";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
-    <header>
-        {/* <div className="container header__container">
-            <h5>Hello I am</h5>
-            <h1>Ornella Borges</h1>
-            <h5 className="text-light">Fullstack Developer</h5>
-            <CTA />
-            <HeaderSocials/>
+    <header className="hero-header">
+      <img className="hero-image-mobile" src={OrnellaMobile} alt="me" />
+      <img className="hero-image-desktop" src={Ornella} alt="me" />
 
-            <div className="me">
-                <img src={OBCOLPNG} alt="me" />
-            </div>
-
-            <a href='#contact' className='scroll__down'>Scroll Down</a>
-        </div> */}
-        <div className="me">
-            <img src={Ornella} alt="me" />
-            <div className='container header__container'>
-                <h5>Hello I am</h5>
-                <h1>Ornella Borges</h1>
-                <h5 className="text-light">Fullstack Developer</h5>
-                <HeaderSocials/>
-                <CTA />
-            </div>
-        </div>
-        
-
+      <div className="hero-text">
+        <h5>Hello I am</h5>
+        <h1>Ornella Borges</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <HeaderSocials />
+        <CTA />
+      </div>
     </header>
-)}
+  );
+};
 
-export default Header
+export default Header;
