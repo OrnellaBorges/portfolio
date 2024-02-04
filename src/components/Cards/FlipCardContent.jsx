89e2e8fd19a4c66d7384e4b2c1427e6f7e3ee69c
Ornella B <borges.ornella@gmail.com>
2024-02-04 20:14:21 +0100

    save

--- src/components/Cards/FlipCardContent.jsx
+++ src/components/Cards/FlipCardContent.jsx
@@ -2,7 +2,15 @@ import React, { useState } from "react";
 import ReactCardFlip from "react-card-flip";
 import "./cardsStyle/styleFlipCard.css";
 
-const FlipCardContent = ({ name, description, poste, date, stacks, id }) => {
+const FlipCardContent = ({
+  name,
+  description,
+  poste,
+  date,
+  stacks,
+  id,
+  image,
+}) => {
   const [isFlipped, setIsFlipped] = useState(false);
 
   const handleClick = () => {
@@ -15,29 +23,32 @@ const FlipCardContent = ({ name, description, poste, date, stacks, id }) => {
         <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
           {/* Front side */}
           <div className="card-front" onClick={handleClick}>
-            <img
-              src="https://i.ibb.co/b22kSx7/marek-pospisil-o-UBjd22g-F6w-unsplash.jpg"
-              alt="orange"
-            />
+            <img src={image} alt="logo de la marque" />
           </div>
           {/* Back side */}
           <div className="card-back" onClick={handleClick}>
-            <h2>{name}</h2>
-            <p>{poste}</p>
-            <small>{date}</small>
-            <p className="flipCard-text">{description}</p>
-            <ul className="stackList">
-              {/* condition ternaire */}
-              {stacks && Array.isArray(stacks) && stacks.length > 0 ? (
-                stacks.map((stack) => (
-                  <li className="stackList-element" key={`${id}-${stack}`}>
-                    {stack}
-                  </li>
-                ))
-              ) : (
-                <li className="message">No stacks available</li>
-              )}
-            </ul>
+            <div className="card-back__content">
+              <h2>{name}</h2>
+              <p>{poste}</p>
+              <small>{date}</small>
+              <p className="flipCard-text">{description}</p>
+            </div>
+
+            <div className="stackContainer">
+              <ul className="stackList">
+                {/* condition ternaire */}
+
+                {stacks && Array.isArray(stacks) && stacks.length > 0 ? (
+                  stacks.map((stack) => (
+                    <li className="stackList-element" key={`${id}-${stack}`}>
+                      {stack}
+                    </li>
+                  ))
+                ) : (
+                  <p className="message">No stacks available</p>
+                )}
+              </ul>
+            </div>
           </div>
         </ReactCardFlip>
       </div>
--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import FlipCardContent from "./Cards/FlipCardContent";
 import "./test.css";
 
+import imagePathSiplec from "../assets/siplec-backgroud.jpeg";
+import imagePathTex from "../assets/tex-card.png";
+import imagePathSarenza from "../assets/sarenza-card.jpeg";
+
 const TestComponent = () => {
   const infosExperience = [
     {
@@ -9,13 +13,38 @@ const TestComponent = () => {
       name: "E.Leclerc",
       poste: "Modéliste 2D/3D",
       date: "2016-2020",
-      description: `Chargée de concevoir les collections Basics de
-                    la marque Tissaia. Création des bases et mise en
+      description: `Chargée de concevoir les collections de
+                    la marque Tissaia. Création des bases, mise en
                     place d'une bibliothèque afin d'optimiser la
                     conceptions des collections futurs. Conception
-                    et mise au point des produits sur Gerber
+                    et mise au point sur Gerber
                     Accumark.`,
       stacks: ["Gerber", "Lectra", "Adobe", "Plm"],
+      pathImage: imagePathSiplec,
+    },
+    {
+      id: 2,
+      name: "TexEurop",
+      poste: "Modéliste 2D/3D",
+      date: "2020-2022",
+      description: `Modélisation 3D les collections pour
+      plusieurs marques via CLO 3D. Mise au point
+      des produits et intermédiaire technique direct
+      entre les usines et les marques.`,
+      stacks: ["Clo 3D", "Marvelous", "Lectra", "Adobe"],
+      pathImage: imagePathTex,
+    },
+    {
+      id: 2,
+      name: "Sarenza",
+      poste: "Intégratrice-Developpeuse",
+      date: "2022-2023",
+      description: `Modélisation 3D les collections pour
+        plusieurs marques via CLO 3D. Mise au point
+        des produits et intermédiaire technique direct
+        entre les usines et les marques.`,
+      stacks: ["HTML", "CSS/SASS", "React", "Redux"],
+      pathImage: imagePathSarenza,
     },
   ];
 
@@ -26,8 +55,27 @@ const TestComponent = () => {
         poste={infosExperience[0].poste}
         description={infosExperience[0].description}
         date={infosExperience[0].date}
-        stack={infosExperience[0].stacks}
+        stacks={infosExperience[0].stacks}
         id={infosExperience[0].id}
+        image={infosExperience[0].pathImage}
+      />
+      <FlipCardContent
+        name={infosExperience[1].name}
+        poste={infosExperience[1].poste}
+        description={infosExperience[1].description}
+        date={infosExperience[1].date}
+        stacks={infosExperience[1].stacks}
+        id={infosExperience[1].id}
+        image={infosExperience[1].pathImage}
+      />
+      <FlipCardContent
+        name={infosExperience[2].name}
+        poste={infosExperience[2].poste}
+        description={infosExperience[2].description}
+        date={infosExperience[2].date}
+        stacks={infosExperience[2].stacks}
+        id={infosExperience[2].id}
+        image={infosExperience[2].pathImage}
       />
     </section>
   );
